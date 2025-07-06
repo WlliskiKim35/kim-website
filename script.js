a7891d25376306f1c86812650c6c343591949f39
WlliskiKim35 <kimwillis024@gmail.com>
2025-07-05 23:06:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.onload = function () {
+  alert("Welcome to Kimberly's Personal Website!");
+};
 
+window.onscroll = function () {
+  const btn = document.getElementById("backToTop");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
